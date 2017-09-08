59f2299d961c9a1010e1c4e364ebe5e4084902a8
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-09-08 11:05:47 +0500

    add ip, os and lang info

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var path = require('path')
+var os = require('os');
 
 var app = express()
 var port = process.env.PORT || 9000
@@ -16,7 +17,15 @@ app.use('*', (req, res, next) => {
 })
 
 app.get('/', (req, res, next) => {
-  res.send('Running!')
+  var data = {
+    ip: req.headers['x-forwarded-for'] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress,
+    os: os.platform() + ', ver: ' + os.release(),
+    lang: req.headers["accept-language"]
+  }
+  res.send(data)
 })
 
 app.use('*', function (req, res) {
